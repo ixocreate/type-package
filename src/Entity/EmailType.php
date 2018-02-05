2357c0cc3d5ad2a2e577641c2c74d35ea0ae6b05
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-02-05 13:41:18 +0100

    initial commit

---
+++ src/Entity/EmailType.php
@@ -0,0 +1,58 @@
+<?php
+namespace KiwiSuite\CommonType\Entity;
+
+use Assert\Assertion;
+use KiwiSuite\Entity\Type\Convert\Convert;
+use KiwiSuite\Entity\Type\TypeInterface;
+
+final class EmailType implements TypeInterface
+{
+    /**
+     * @var string
+     */
+    private $value;
+
+    /**
+     * EmailType constructor.
+     * @param string $value
+     * @throws \Assert\AssertionFailedException
+     */
+    public function __construct(string $value)
+    {
+        Assertion::email($value);
+        $this->value = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getInternalType(): string
+    {
+        return TypeInterface::TYPE_STRING;
+    }
+
+    /**
+     * @param $value
+     * @return mixed
+     */
+    public static function convertToInternalType($value)
+    {
+        return Convert::convertString($value);
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->value;
+    }
+}
---
+++ src/Entity/UuidType.php
@@ -0,0 +1,52 @@
+<?php
+namespace KiwiSuite\CommonType\Entity;
+
+use Assert\Assertion;
+use KiwiSuite\Entity\Type\Convert\Convert;
+use KiwiSuite\Entity\Type\TypeInterface;
+use Ramsey\Uuid\Uuid;
+
+final class UuidType implements TypeInterface
+{
+    /**
+     * @var Uuid
+     */
+    private $uuid;
+
+    public function __construct(string $value)
+    {
+        Assertion::uuid($value);
+
+        $this->uuid = Uuid::fromString($value);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->uuid->toString();
+    }
+
+    /**
+     * @return string
+     */
+    public static function getInternalType(): string
+    {
+        return TypeInterface::TYPE_STRING;
+    }
+
+    /**
+     * @param $value
+     * @return mixed
+     */
+    public static function convertToInternalType($value)
+    {
+        return Convert::convertString($value);
+    }
+
+    public function __toString()
+    {
+        return $this->uuid->toString();
+    }
+}
