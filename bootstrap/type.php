f416df4f04c910e40f9b7cc20e50339b4f08c087
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-06-14 16:35:05 +0200

    added missing file

--- bootstrap/type.php
+++ bootstrap/type.php
@@ -1,10 +1,15 @@
 <?php
 namespace KiwiSuite\CommonTypes;
 
+use KiwiSuite\CommonTypes\Entity\BlockContainerType;
+use KiwiSuite\CommonTypes\Entity\BlockType;
+use KiwiSuite\CommonTypes\Entity\CollectionType;
 use KiwiSuite\CommonTypes\Entity\ColorType;
 use KiwiSuite\CommonTypes\Entity\DateTimeType;
 use KiwiSuite\CommonTypes\Entity\DateType;
 use KiwiSuite\CommonTypes\Entity\EmailType;
+use KiwiSuite\CommonTypes\Entity\Factory\SchemaTypeFactory;
+use KiwiSuite\CommonTypes\Entity\SchemaType;
 use KiwiSuite\CommonTypes\Entity\UuidType;
 use KiwiSuite\Entity\Type\TypeConfigurator;
 
@@ -14,3 +19,7 @@ $type->addType(ColorType::class);
 $type->addType(UuidType::class);
 $type->addType(DateTimeType::class);
 $type->addType(DateType::class);
+$type->addType(CollectionType::class);
+$type->addType(BlockContainerType::class);
+$type->addType(BlockType::class);
+$type->addType(SchemaType::class, SchemaTypeFactory::class);
